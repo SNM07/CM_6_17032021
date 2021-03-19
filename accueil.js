e42c2643d74b0e729a6615b0d482b3923e032588
SNM07 <say.no.more@hotmail.fr>
2021-03-19 11:07:54 +0100

    Build v1-b

--- accueil.js
+++ accueil.js
@@ -1,4 +1,4 @@
-fetch("./FishEyeDataFR.json")
+/*fetch("./FishEyeDataFR.json")
 .then(response => {
    return response.json();
 })
@@ -29,3 +29,73 @@ fetch("./FishEyeDataFR.json")
     
         ${photographers.map(profileTemplate).join("")}
       `;
+      */
+      // const header = document.querySelector('header');
+      const section = document.querySelector('profiles');
+  
+      let requestURL = './FishEyeDataFR.json';
+      let request = new XMLHttpRequest();
+      request.open('GET', requestURL);
+      request.responseType = 'json';
+      request.send();
+  
+      request.onload = function() {
+        const profileTemplate = request.response;
+        // populateHeader(profileTemplate);
+        showProfile(profileTemplate);
+      }
+  
+      /* function populateHeader(obj) {
+        const myH1 = document.createElement('h1');
+        myH1.textContent = obj['squadName'];
+        header.appendChild(myH1);
+  
+        const myPara = document.createElement('p');
+        myPara.textContent = 'Hometown: ' + obj['homeTown'] + ' // Formed: ' + obj['formed'];
+        header.appendChild(myPara);
+      } */
+  
+      function showProfile(obj) {
+        const photog = obj['photographers'];
+  
+        for(let i = 0; i < photog.length; i++) {
+          const myArticle = document.createElement('article');
+          const myImg = document.createElement('img');
+          const myH2 = document.createElement('h2');
+          const myPara1 = document.createElement('p');
+          const myPara2 = document.createElement('p');
+          const myPara3 = document.createElement('p');
+          const myTags = document.createElement('ul');
+          
+          myArticle.setAttribute("class", "profileCard");
+          myImg.setAttribute("class", "profilePic");
+          myH2.setAttribute("class", "profileName");
+          myPara1.setAttribute("class", "profileLocation");
+          myPara2.setAttribute("class", "profileTagline");
+          myPara3.setAttribute("class", "profilePrice");
+          myTags.setAttribute("class", "profileTags");
+  
+          myImg.src = ("./images/Photographers-ID-Photos/" + photog[i].portrait);
+          myH2.textContent = photog[i].name;
+          myPara1.textContent = photog[i].city + ', ' + photog[i].country;
+          myPara2.textContent = photog[i].tagline;
+          myPara3.textContent = photog[i].price + '€/jour';
+          
+  
+          const catTags = photog[i].tags;
+          for(let j = 0; j < catTags.length; j++) {
+            const listTags = document.createElement('li');
+            listTags.textContent = catTags[j];
+            myTags.appendChild(listTags);
+          } 
+  
+          myArticle.appendChild(myImg);
+          myArticle.appendChild(myH2);
+          myArticle.appendChild(myPara1);
+          myArticle.appendChild(myPara2);
+          myArticle.appendChild(myPara3);
+          myArticle.appendChild(myTags);
+  
+          profiles.appendChild(myArticle);
+        }
+      }
