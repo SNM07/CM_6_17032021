f3cd1fa801f13393714352e15ab65e3cf2a1b8ad
SNM07 <say.no.more@hotmail.fr>
2021-03-21 15:57:57 +0100

    Build v2-b

--- accueil.js
+++ accueil.js
@@ -209,7 +209,7 @@ mybutton = document.getElementById("contentButton");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
