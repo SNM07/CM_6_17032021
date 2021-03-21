48a7c97dfb0e94a838f49567014c948a1d943f93
SNM07 <say.no.more@hotmail.fr>
2021-03-21 15:32:51 +0100

    Build v2

--- accueil.js
+++ accueil.js
@@ -169,6 +169,53 @@ for (var i = 0; i < btns.length; i++) {
           );
           profiles.append(article);
         } */
+        const body = document.body;
+        // const menu = document.querySelector(".page-header");
+        /* const nav = document.querySelector(".page-header nav");
+        const menu = document.querySelector(".page-header .menu"); */
+        const scrollUp = "scroll-up";
+        const scrollDown = "scroll-down";
+        let lastScroll = 0;
+        
+        /* menu.addEventListener("click", () => {
+          body.classList.toggle("menu-open");
+        }); */
+        
+        window.addEventListener("scroll", () => {
+          const currentScroll = window.pageYOffset;
+          if (currentScroll <= 0) {
+            body.classList.remove(scrollUp);
+            body.classList.remove(scrollDown);
+            return;
+          }
+          
+          if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+            // down
+            body.classList.remove(scrollUp);
+            body.classList.add(scrollDown);
+          } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+            // up
+            body.classList.remove(scrollDown);
+            body.classList.add(scrollUp);
+          }
+          lastScroll = currentScroll;
+        });
+      
+      
+      //Get the button:
+mybutton = document.getElementById("contentButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
     });
 
 
@@ -235,3 +282,13 @@ for (var i = 0; i < btns.length; i++) {
   });
   
 } */
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  /* if (document.body.classList.contains("scrollDown")){
+    document.body.classList.remove("scrollDown");
+    document.body.classList.add("scrollUp");
+  } */
+}
