a1694ec293f9f645e60001cbf3f1da21f538b086
SNM07 <say.no.more@hotmail.fr>
2021-04-20 15:42:42 +0200

    Build v16-e

--- photographer-page.js
+++ photographer-page.js
@@ -958,6 +958,7 @@ document
 function topFunction2() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  window.location.reload()
 }
 
 
