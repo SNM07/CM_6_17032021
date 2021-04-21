bb9223d6b6b69ae3c5ac75ec4e272c7c7d132b29
SNM07 <say.no.more@hotmail.fr>
2021-04-21 16:02:10 +0200

    Build v18-b
    TEST FONCTION

--- photographer-page.js
+++ photographer-page.js
@@ -356,6 +356,8 @@ fetch("./FishEyeDataFR.json")
 
   .then((object) => {
     showProfile(object);
+    //showProfile2(object, mapJSON, contactModal);
+    
 
     showGallery(object);
 
@@ -636,3 +638,133 @@ window.onload = function () {
     tabIndex++;
   });
 }); */
+
+
+
+
+
+////////////////TEST FONCTION
+/* 
+//Construct photographer profile
+function showProfile2(object, mapJSON, contactModal) {
+  fetch("./FishEyeDataFR.json")
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((object) => {
+      //Get JSON data
+      const photog = object["photographers"];
+      mapJSON(photog);
+      contactModal(photog);
+      console.log(mapJSON, contactModal);
+  
+      for (let i = 0; i < photog.length; i++) {
+        //Create elements
+        const myArticle = document.createElement("article");
+        const myImg = document.createElement("img");
+        const myH1 = document.createElement("h1");
+        const myPara1 = document.createElement("p");
+        const myPara2 = document.createElement("p");
+        const myPara3 = document.createElement("p");
+        const myTags = document.createElement("div");
+    
+        //Card attributes
+        myArticle.setAttribute("class", "profileCardPP");
+    
+        //Image attributes
+        myImg.setAttribute("class", "profilePic");
+        myImg.setAttribute("alt", photog[i].name + " profile picture");
+        myImg.src = "./images/Photographers-ID-Photos/" + photog[i].portrait;
+    
+        //Name attributes
+        myH1.setAttribute("class", "profileName");
+        myH1.textContent = photog[i].name;
+    
+        //Location attributes
+        myPara1.setAttribute("class", "profileLocation");
+        myPara1.textContent = photog[i].city + ", " + photog[i].country;
+    
+        //Tagline attributes
+        myPara2.setAttribute("class", "profileTagline");
+        myPara2.textContent = photog[i].tagline;
+    
+        //Price attributes
+        myPara3.setAttribute("class", "profilePrice");
+        myPara3.textContent = photog[i].price + "€/jour";
+    
+        //Tags attributes
+        myTags.setAttribute("class", "profileTags");
+        const catTags = photog[i].tags;
+        for (let j = 0; j < catTags.length; j++) {
+          const listTags = document.createElement("button");
+          listTags.title = catTags[j];
+          listTags.setAttribute("data-filter", "." + catTags[j]);
+          listTags.setAttribute("tabindex", "0");
+          listTags.textContent = "# " + catTags[j];
+          myTags.appendChild(listTags);
+          const profileCardClass = "profileCardPP";
+          myArticle.setAttribute(
+            "class",
+            profileCardClass + " " + catTags.join(" ")
+          );
+        }
+      
+        //Delete profiles of other photographers
+        let x = profilID[i];
+        let y = profURL;
+        let currentName = null;
+      
+        if (x == y) {
+          myArticle.style.display = "true";
+          currentName = photog[i].name;
+        } else {
+          myArticle.style.display = "none";
+          myArticle.classList.add("Delete");
+          document.querySelectorAll(".Delete").forEach((e) => e.remove());
+        }
+      
+        //Append Elements
+        myArticle.appendChild(myImg);
+        myArticle.appendChild(myH1);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myTags);
+      
+        profilesPP.appendChild(myArticle);
+      }
+    }
+    );
+}
+
+  
+  //Map JSON data
+  mapJSON();
+function mapJSON(photog) {
+  
+      const profilID = photog.map((photog) => photog.id);
+      console.log(profilID)
+    }
+    
+  
+  
+  //Contact button construction
+  contactModal();
+  function contactModal(photog) {
+    //photog = this.photog;
+    const myContactContainer = document.createElement("div");
+    const myContact = document.createElement("button");
+    const contactText = document.createTextNode("Contactez-moi");
+    myContactContainer.setAttribute("class", "contactFormButton");
+    myContactContainer.setAttribute("alt", "Contact me");
+    myContact.setAttribute("class", "modal-btn");
+    myContact.appendChild(contactText);
+    myContactContainer.appendChild(myContact);
+    profilesPP.appendChild(myContactContainer);
+}
+
+
+      
+  
+ */
