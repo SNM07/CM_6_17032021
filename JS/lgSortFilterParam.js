1a398af28bd2c8ba2344b3a8c44eafd49a3db1b3
SNM07 <say.no.more@hotmail.fr>
2021-04-30 11:15:47 +0200

    Build v23-g

--- JS/lgSortFilterParam.js
+++ JS/lgSortFilterParam.js
@@ -9,22 +9,21 @@ export default function sortAndFilterParam() {
     hideBarsDelay: 0,
     counter: false,
   });
-///////////////////
+  ///////////////////
   if (window.innerWidth < 800) {
     var $grid = $(".photoGall").isotope({
       itemSelector: ".photoAHREF",
       layoutMode: "vertical",
-  
+
       fitRows: {
         columnWidth: 50,
         gutter: 30,
       },
       getSortData: {
-
         filterValue: ".photoTags",
       },
     });
-  
+
     $(".profileTags").on("click", "button", function (event) {
       var $target = $(event.currentTarget);
       $target.toggleClass("is-checked");
@@ -35,16 +34,14 @@ export default function sortAndFilterParam() {
       } else {
         removeFilter(filter);
       }
- 
+
       let filterTags = filters.join(",");
- 
+
       $grid.isotope({ filter: filterTags });
     });
   }
   //////////////////////
-  /* $gallery.on('onAferAppendSlide.lg',function(event, index, dynamicEl, nextHtml, appendSubHtmlTo, fromTouch, fromThumb){
-    console.log(index, dynamicEl, nextHtml, appendSubHtmlTo, fromTouch, fromThumb);
-}); */
+
   //Isotope Sorting & Filtering for desktop view
   if (window.innerWidth >= 800) {
     // init Isotope
@@ -100,116 +97,75 @@ export default function sortAndFilterParam() {
       var filter = $target.attr("data-filter");
       if (isChecked) {
         addFilter(filter);
-        //tabFilter();
       } else {
         removeFilter(filter);
-        //tabFilter();
       }
-      /* function tabFilter() {
-        let $cont = $('.photoAHREF');
-
-        $cont.each(function () {
-          if ($cont.css('display') == 'none') {
-            $cont.attr("tabindex", "-1");
-          } else if ($cont.css('display') == 'block') {
-            $cont.attr("tabindex", "0");
-          }
-        }
-        );
-      } */
 
       // filter isotope
       // group filters together, inclusive
       let filterTags = filters.join(",");
-      /*if (filters.length > 0) {
-        filterTags = filters.join(",");
-      } */
+
       $grid.isotope({ filter: filterTags });
 
-      //let filterClass = filter.replace(".", "");
-      //rearrange after filtering
-      //if (filters.length > 0) {
+      //recreate lightbox + HTML order after filter
       $grid.on("arrangeComplete", function () {
         sortOut();
-        $gallery.data("lightGallery").destroy(true);
-        $gallery.lightGallery({
-          selector: filters[0].replace("*", ""),
-          download: false,
-          getCaptionFromTitleOrAlt: true,
-          preload: 2,
-          fullScreen: true,
-          hideBarsDelay: 0,
-          counter: false,
-        });
+        destroyGal($gallery, filters);
+        createFiltGal($gallery, filters);
       });
-      //};
     });
-
+    
     function addFilter(filter) {
       if (filters.indexOf(filter) == -1) {
         filters.push(filter);
       }
     }
-
+    
     function removeFilter(filter) {
       var index = filters.indexOf(filter);
       if (index != -1) {
         filters.splice(index, 1);
       }
     }
-
+    
     //Test tags focus keyboard...
     $grid
-      .on("arrangeComplete", function (e, filteredItems) {
-        console.log("arrangeComplete");
-
-        var dataVal = 1;
-
-        $(filteredItems).each(function (index, item) {
-          $(item.element).find("a.title").attr("data-val", dataVal);
-          let t = dataVal++;
-          index = t.toLocaleString("en-US", {
-            minimumIntegerDigits: 2,
-            useGrouping: false,
-          });
-          item.element.setAttribute("data-val", index);
+    .on("arrangeComplete", function (e, filteredItems) {
+      console.log("arrangeComplete");
+      
+      var dataVal = 1;
+      
+      $(filteredItems).each(function (index, item) {
+        $(item.element).find("a.title").attr("data-val", dataVal);
+        let t = dataVal++;
+        index = t.toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
         });
-
-        //sortOut();
-        /* $gallery.data("lightGallery").destroy(true);
-        $gallery.lightGallery({
-          download: false,
-          getCaptionFromTitleOrAlt: true,
-          preload: 2,
-          fullScreen: true,
-          hideBarsDelay: 0,
-          counter: false,
-        }); */
-        console.log("1");
-        //tabFilter();
-      })
-      .isotope();
-
+        item.element.setAttribute("data-val", index);
+      });
+      
+      console.log("1");
+    })
+    .isotope();
+    
     $grid
-      .on("arrangeComplete", function () {
-        let $cont = $('a[style*="block"]');
-        let $cont2 = $('a[style*="display: none"]');
-        let $contAll = $(".photoAHREF");
-        $contAll.each(function () {
-          $contAll.attr("tabindex", "0");
-
-        });
-
-        $contAll.each(function () {
-          $cont2.attr("tabindex", "-1");
-
-        });
-        $contAll.each(function () {
-          $cont.attr("tabindex", "0");
-        });
-
-      })
-      .isotope();
+    .on("arrangeComplete", function () {
+      let $cont = $('a[style*="block"]');
+      let $cont2 = $('a[style*="display: none"]');
+      let $contAll = $(".photoAHREF");
+      $contAll.each(function () {
+        $contAll.attr("tabindex", "0");
+      });
+      
+      $contAll.each(function () {
+        $cont2.attr("tabindex", "-1");
+      });
+      $contAll.each(function () {
+        $cont.attr("tabindex", "0");
+      });
+    })
+    .isotope();
   }
 }
 
@@ -219,16 +175,36 @@ function sortOut() {
   for (var i = 0; i < classname.length; ++i) {
     divs.push(classname[i]);
   }
-  //let ts = tabIndex.toString();
+  
   divs.sort(function (a, b) {
     return a.dataset.val.localeCompare(b.dataset.val);
   });
-
+  
   var br = document.getElementsByTagName("br")[0];
   let parent = document.getElementById("photoGallery");
-
+  
   divs.forEach(function (el) {
     parent.insertBefore(el, br);
   });
 }
 ////////////////
+
+function destroyGal($gallery, filters) {
+  if (filters.length > 0) {
+    $gallery.data("lightGallery").destroy(true);
+  }
+}
+
+function createFiltGal($gallery, filters) {
+  if (filters.length > 0) {
+    $gallery.lightGallery({
+      selector: filters[0].replace("*", ""),
+      download: false,
+      getCaptionFromTitleOrAlt: true,
+      preload: 2,
+      fullScreen: true,
+      hideBarsDelay: 0,
+      counter: false,
+    });
+  }
+}
